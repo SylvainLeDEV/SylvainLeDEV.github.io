81b584f1397f51a3f190d8a023f724c4c1e8db60
Sylvain <dev.tess.sylvain@gmail.com>
2022-04-26 16:58:11 +0200

    fix nav and header.

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,31 @@
-
 // Curtain menu
 function openNav() {
     document.getElementById("myOverlay").style.height = "100%";
 }
+
 function closeNav() {
     document.getElementById("myOverlay").style.height = "0%";
 }
 
+// Navbar
+let lastScrollTop = 0;
+navbar = document.getElementById("myNavbar");
+window.addEventListener("scroll", function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        navbar.style.opacity = "0";
+        navbar.style.height = "0px";
+    } else {
+        navbar.style.opacity = "0.98";
+        navbar.style.height = "57px";
+    }
 
+    lastScrollTop = scrollTop;
 
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const hero = document.getElementById('hero');
+console.log(hero);
 
 
 
