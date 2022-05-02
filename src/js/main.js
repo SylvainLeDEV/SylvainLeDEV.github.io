f2a68185aaf804ced39b2533b07deadbc114d54e
Sylvain <dev.tess.sylvain@gmail.com>
2022-05-02 12:35:36 +0200

    autoPlay OK + Carousel OK

--- src/js/main.js
+++ src/js/main.js
@@ -26,7 +26,7 @@ window.addEventListener("scroll", function (e) {
 
 
 //--------- Carousel ------//
-document.body.onload = function () {
+document.body.onload = function carousel() {
 
     const buttonRight = document.querySelector(".carousel__arrow-right");
     const buttonLeft = document.querySelector(".carousel__arrow-left");
@@ -34,8 +34,20 @@ document.body.onload = function () {
 
     let slideIndex = 0;
     let numSlides = 4;
-    
+
+    // Check clientWidth;
     let clientWidth = slides.clientWidth;
+
+    // AutoPlay
+    const autoPlay = setInterval(() => {
+        slideIndex++;
+        if (slideIndex > numSlides - 1 ) {
+            slideIndex = 0;
+        }
+        slides.style.transform = `translateX(-${slideIndex * clientWidth}px)`;
+        slides.style.transition = "all 0.6s ease-in-out";
+    }, 5000);
+
     slides.style.width = (numSlides * 100) + "%";
 
     for (let i = 0; i < numSlides; i++) {
@@ -44,20 +56,33 @@ document.body.onload = function () {
         image.style.backgroundImage = `url(./assets/img/carousel/carousel${i + 1}.jpg)`;
         slides.appendChild(image);
     }
-    buttonLeft.addEventListener("click", function () {
+
+    buttonLeft.onclick = function () {
         slideIndex--;
         if (slideIndex < 0) {
             slideIndex = numSlides - 1;
         }
+        console.log('left', slideIndex);
         slides.style.transform = `translate(-${slideIndex * clientWidth}px)`;
-    });
-    buttonRight.addEventListener("click", function () {
+        slides.style.transition = "all 0.6s ease-in-out";
+        clearInterval(autoPlay);
+    };
+
+    buttonRight.onclick = function click() {
         slideIndex++;
         if (slideIndex > numSlides - 1) {
             slideIndex = 0;
         }
+        console.log('right', slideIndex);
         slides.style.transform = `translate(-${slideIndex * clientWidth}px)`;
-    });
+        slides.style.transition = "all 0.6s ease-in-out";
+        clearInterval(autoPlay);
+    };
+
+
+    window.onresize = function () {
+        return window.location.reload();
+    }
 };
 
 //----- END CAROUSEL -----//
