f01bbde92ff2d690d9735fbc58ac65406538a6d1
Sylvain <dev.tess.sylvain@gmail.com>
2022-05-04 19:34:34 +0200

    fix bug carousel ✅

--- src/js/main.js
+++ src/js/main.js
@@ -31,43 +31,59 @@ document.body.onload = function carousel() {
     const buttonLeft = document.querySelector(".carousel__arrow-left");
     const slides = document.getElementById("carousel-items");
 
+
+    const linkOfProject = [
+        {
+            nom: "Reservia",
+            URL : "https://sylvainledev.github.io/TessierSylvain_2_26072021/#cards_hebergements"
+        },
+        {
+            nom: "OhMyFood",
+            URL : "https://sylvainledev.github.io/SylvainTessier_3_20082021/"
+        },
+        {
+            nom: "Groupomania",
+            URL : "https://github.com/SylvainLeDEV/TessierSylvain_7_16012022"
+        },
+        {
+            nom: "HotTakes",
+            URL : "https://github.com/SylvainLeDEV/TESSIERSylvain_6_23112021"
+        }
+    ];
+
     let slideIndex = 0;
-    let numSlides = 4;
+    let numSlides = linkOfProject.length;
 
     // Check clientWidth;
-    let clientWidth = slides.clientWidth;
+    let containerCarousel = document.querySelector(".portfolio__container-carousel").clientWidth;
+    window.onresize = function () {
+        containerCarousel = document.querySelector(".portfolio__container-carousel").clientWidth;
+        slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
+    }
+    slides.style.width = (numSlides * 100) + "%";
 
-    // AutoPlay
+
     const autoPlay = setInterval(() => {
         slideIndex++;
-        if (slideIndex > numSlides - 1 ) {
+        if (slideIndex > numSlides - 1) {
             slideIndex = 0;
         }
-        slides.style.transform = `translateX(-${slideIndex * clientWidth}px)`;
+        slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
         slides.style.transition = "all 0.6s ease-in-out";
     }, 3500);
 
-    slides.style.width = (numSlides * 100) + "%";
-
-    const linkOfProject = {
-        "1" : "https://sylvainledev.github.io/TessierSylvain_2_26072021/#cards_hebergements",
-        "2" : "https://sylvainledev.github.io/SylvainTessier_3_20082021/",
-        "3" : "https://github.com/SylvainLeDEV/TessierSylvain_7_16012022",
-        '4' : "https://github.com/SylvainLeDEV/TESSIERSylvain_6_23112021"
-    }
 
     for (let i = 0; i < numSlides; i++) {
         // Create a new div'image' element for each slide
         const image = document.createElement("div");
         image.className = "carousel__image";
-
         image.style.backgroundImage = `url(./assets/img/carousel/carousel${i + 1}.jpg)`;
         slides.appendChild(image);
 
         //For each image, create a link to the project
         const anchor = document.createElement("a");
         anchor.className = "carousel__anchor";
-        anchor.href = Object.values(linkOfProject)[i];
+        anchor.href = linkOfProject[i].URL;
         anchor.target = "_blank";
         image.appendChild(anchor);
 
@@ -77,14 +93,12 @@ document.body.onload = function carousel() {
         // anchor.appendChild(projectName);
 
     }
-
     buttonLeft.onclick = function () {
         slideIndex--;
         if (slideIndex < 0) {
             slideIndex = numSlides - 1;
         }
-        console.log('left', slideIndex);
-        slides.style.transform = `translate(-${slideIndex * clientWidth}px)`;
+        slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
         slides.style.transition = "all 0.6s ease-in-out";
         clearInterval(autoPlay);
     };
@@ -94,23 +108,43 @@ document.body.onload = function carousel() {
         if (slideIndex > numSlides - 1) {
             slideIndex = 0;
         }
-        console.log('right', slideIndex);
-        slides.style.transform = `translate(-${slideIndex * clientWidth}px)`;
+        slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
         slides.style.transition = "all 0.6s ease-in-out";
         clearInterval(autoPlay);
+
+        setTimeout(() => {
+            autoPlay();
+        })
+
     };
 
 
-    window.onresize = function () {
-        return window.location.reload();
-    }
 };
 
 //----- END CAROUSEL -----//
 
 
+//----- TypedJS -----//
+
+// import Typed from '/assets/typed.js/lib/typed.js';
+let typed = new Typed('.typed', {
+    strings: ["Java Script", "React JS", "Node JS", "HTML / CSS"],
+    typeSpeed: 40,
+    backSpeed: 100,
+    fadeOut: true,
+    loop: true
+});
+//----- END TypedJS -----//
+
+
 // AOS animation library JS
 AOS.init({
-    duration: 1200,
+    startEvent: 'DOMContentLoaded',
+    disable: 'mobile',
+    duration: 1100,
 });
+// END AOS animation library JS--//
+
+console.log("COUCOU :) ! Si tu est là, c'est que tu inspecte mon site, pas de problème a ça !  ");
+console.log("Le code est en open source n'hésite pas à t'en servire si tu en as besoin ! ");
 
