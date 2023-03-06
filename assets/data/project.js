d24388195d24863876858c3f3911c7a6738e1fcd
SylvainLeDev <dev.tess.sylvain@gmail.com>
2023-03-06 11:50:46 +0100

    update CV - improve JS - add my notion

--- assets/data/project.js
+++ assets/data/project.js
@@ -1,18 +1,29 @@
-export const linkOfProject = [
+export const PROJECTS = [
     {
         nom: "Reservia",
-        URL: "https://sylvainledev.github.io/TessierSylvain_2_26072021/#cards_hebergements"
+        URL: "https://sylvainledev.github.io/TessierSylvain_2_26072021/#cards_hebergements",
+        IMG : "Reservia"
     },
     {
         nom: "OhMyFood",
-        URL: "https://sylvainledev.github.io/SylvainTessier_3_20082021/"
+        URL: "https://sylvainledev.github.io/SylvainTessier_3_20082021/",
+        IMG: "OhMyFood"
     },
     {
         nom: "Groupomania",
-        URL: "https://github.com/SylvainLeDEV/TessierSylvain_7_16012022"
+        URL: "https://github.com/SylvainLeDEV/TessierSylvain_7_16012022",
+        IMG : "HotTakes"
     },
     {
         nom: "HotTakes",
-        URL: "https://github.com/SylvainLeDEV/TESSIERSylvain_6_23112021"
-    }
+        URL: "https://github.com/SylvainLeDEV/TESSIERSylvain_6_23112021",
+        IMG : "Groupomania"
+    },
+    {
+        nom: "MyWebSite",
+        URL: "https://github.com/SylvainLeDEV/SylvainLeDev.github.io",
+        IMG : "Hero"
+    },
+
+
 ];
--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,29 @@ function openNav() {
     document.getElementById("myOverlay").style.height = "100%";
 }
 
+const openCurtain = document.querySelector(".open-curtain")
+openCurtain.addEventListener('click', openNav)
+
 function closeNav() {
     document.getElementById("myOverlay").style.height = "0%";
 }
 
+const closeLinks = document.querySelectorAll('.close-curtain');
+// Get all links with the class "close-curtain"
+closeLinks.forEach(link => {
+    // For each link, add an event listener for when it's clicked
+    link.addEventListener('click', e => {
+        // When the link is clicked, do the following:
+        e.preventDefault(); // Prevent the default behavior of the browser
+        closeNav(); // Close the menu
+        const targetId = e.target.getAttribute('href');
+        // Get the ID of the target element from the link's "href" attribute
+        const targetElement = document.querySelector(targetId);
+        // Find the element with that ID
+        targetElement.scrollIntoView({behavior: 'smooth'});
+        // Scroll the page to that element
+    });
+});
 // ---- Navbar Scroll --------//
 let lastScrollTop = 0;
 const navbar = document.getElementById("myNavbar");
@@ -25,8 +44,8 @@ window.addEventListener("scroll", function (e) {
 // ---- End Navbar ----------//
 
 // ---- Description ---------//
-import { DESCRIPTION } from "/SylvainLeDev.github.io/assets/data/description.js";
-console.log(DESCRIPTION)
+import {DESCRIPTION} from "/SylvainLeDev.github.io/assets/data/description.js";
+
 const descriptionBlock = document.querySelector(".block__text");
 for (let i = 0; i < DESCRIPTION.length; i++) {
     const paragraph = document.createElement("p");
@@ -34,7 +53,8 @@ for (let i = 0; i < DESCRIPTION.length; i++) {
     descriptionBlock.appendChild(paragraph);
 }
 // ---- End Description -----//
-import { LANGUAGE, TOOLS, SOFT_SKILLS } from "/SylvainLeDev.github.io/assets/data/skills.js"
+import {LANGUAGE, TOOLS, SOFT_SKILLS} from "/SylvainLeDev.github.io/assets/data/skills.js"
+
 const skillsContainer = document.querySelector(".skills__container-cards");
 const generateSkills = (skillsArray, skillClass, title) => {
     const skillDiv = document.createElement("div");
@@ -56,7 +76,8 @@ generateSkills(SOFT_SKILLS, "skill-soft-skills", "Soft Skills");
 // ---- Pill of Skills ------//
 
 // ---- Experience --------- //
-import { CHRONOLOGIE } from "/SylvainLeDev.github.io/assets/data/chronologie.js";
+import {CHRONOLOGIE} from "/SylvainLeDev.github.io/assets/data/chronologie.js";
+
 const timelineDiv = document.querySelector(".timeline");
 // Loop to display each element of CHRONOLOGIE.
 for (let i = 0; i < CHRONOLOGIE.length; i++) {
@@ -92,42 +113,124 @@ for (let i = 0; i < CHRONOLOGIE.length; i++) {
 // ---- Experience ---------//
 
 //--------- Carousel ------//
-document.body.onload = function carousel() {
+// import { PROJECTS } from "/SylvainLeDev.github.io/assets/data/project.js";
+// document.body.onload = function carousel() {
+//
+//     const buttonRight = document.querySelector(".carousel__arrow-right");
+//     const buttonLeft = document.querySelector(".carousel__arrow-left");
+//     const slides = document.getElementById("carousel-items");
+//
+//     let slideIndex = 0;
+//     let numSlides = PROJECTS.length;
+//
+//     // Check clientWidth;
+//     let containerCarousel = document.querySelector(".portfolio__container-carousel").clientWidth;
+//     window.onresize = function () {
+//         containerCarousel = document.querySelector(".portfolio__container-carousel").clientWidth;
+//         slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
+//     }
+//     slides.style.width = (numSlides * 100) + "%";
+//
+//
+//     const autoPlay = () => {
+//         slideIndex++;
+//         if (slideIndex > numSlides - 1) {
+//             slideIndex = 0;
+//         }
+//         slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
+//         slides.style.transition = "all 0.6s ease-in-out";
+//     }
+//
+//     const autoscrolling = setInterval(autoPlay, 3000)
+//
+//
+//     for (let i = 0; i < numSlides; i++) {
+//         // Create a new div'image' element for each slide
+//         const image = document.createElement("div");
+//         image.className = "carousel__image";
+//         image.style.backgroundImage = `url(./assets/img/carousel/carousel${i + 1}.jpg)`;
+//         slides.appendChild(image);
+//
+//         //For each image, create a link to the project
+//         const anchor = document.createElement("a");
+//         anchor.className = "carousel__anchor";
+//         anchor.href = PROJECTS[i].URL;
+//         anchor.target = "_blank";
+//         image.appendChild(anchor);
+//
+//         //For each image indicate the project name if I need it
+//         // const projectName = document.createElement("p");
+//         // projectName.innerHTML = Object.keys(PROJECTS)[i];
+//         // anchor.appendChild(projectName);
+//
+//     }
+//     buttonLeft.onclick = function () {
+//         slideIndex--;
+//         if (slideIndex < 0) {
+//             slideIndex = numSlides - 1;
+//         }
+//         slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
+//         slides.style.transition = "all 0.6s ease-in-out";
+//         clearInterval(autoscrolling);
+//
+//     };
+//
+//     buttonRight.onclick = function click() {
+//         slideIndex++;
+//         if (slideIndex > numSlides - 1) {
+//             slideIndex = 0;
+//         }
+//         slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
+//         slides.style.transition = "all 0.6s ease-in-out";
+//         clearInterval(autoscrolling);
+//     };
+//
+//
+// };
+import {PROJECTS} from "/SylvainLeDev.github.io/assets/data/project.js";
+
+document.addEventListener("DOMContentLoaded", () => {
 
     const buttonRight = document.querySelector(".carousel__arrow-right");
     const buttonLeft = document.querySelector(".carousel__arrow-left");
     const slides = document.getElementById("carousel-items");
-    const linkOfProject = [
-        {
-            nom: "Reservia",
-            URL: "https://sylvainledev.github.io/TessierSylvain_2_26072021/#cards_hebergements"
-        },
-        {
-            nom: "OhMyFood",
-            URL: "https://sylvainledev.github.io/SylvainTessier_3_20082021/"
-        },
-        {
-            nom: "Groupomania",
-            URL: "https://github.com/SylvainLeDEV/TessierSylvain_7_16012022"
-        },
-        {
-            nom: "HotTakes",
-            URL: "https://github.com/SylvainLeDEV/TESSIERSylvain_6_23112021"
-        }
-    ];
 
     let slideIndex = 0;
-    let numSlides = linkOfProject.length;
+    const numSlides = PROJECTS.length;
 
-    // Check clientWidth;
+    // Get the width of the carousel container
     let containerCarousel = document.querySelector(".portfolio__container-carousel").clientWidth;
-    window.onresize = function () {
+
+    // Update the carousel container width on window resize
+    window.addEventListener("resize", () => {
         containerCarousel = document.querySelector(".portfolio__container-carousel").clientWidth;
         slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
-    }
-    slides.style.width = (numSlides * 100) + "%";
+    });
 
+    // Set the width of the carousel to hold all slides
+    slides.style.width = `${numSlides * 100}%`;
 
+    // Create carousel slides and anchors dynamically
+    PROJECTS.forEach((project, i) => {
+        const image = document.createElement("div");
+        image.className = "carousel__image";
+        image.style.backgroundImage = `url(./assets/img/carousel/${project.IMG}.jpg)`;
+
+        const anchor = document.createElement("a");
+        anchor.className = "carousel__anchor";
+        anchor.href = project.URL;
+        anchor.target = "_blank";
+        image.appendChild(anchor);
+
+        // Uncomment below to add project name to the anchor
+        // const projectName = document.createElement("p");
+        // projectName.innerHTML = Object.keys(PROJECTS)[i];
+        // anchor.appendChild(projectName);
+
+        slides.appendChild(image);
+    });
+
+    // Function to play the carousel automatically
     const autoPlay = () => {
         slideIndex++;
         if (slideIndex > numSlides - 1) {
@@ -135,32 +238,12 @@ document.body.onload = function carousel() {
         }
         slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
         slides.style.transition = "all 0.6s ease-in-out";
-    }
+    };
 
-    const autoscrolling = setInterval(autoPlay, 3000)
+    let autoscrolling = setInterval(autoPlay, 3000);
 
-
-    for (let i = 0; i < numSlides; i++) {
-        // Create a new div'image' element for each slide
-        const image = document.createElement("div");
-        image.className = "carousel__image";
-        image.style.backgroundImage = `url(./assets/img/carousel/carousel${i + 1}.jpg)`;
-        slides.appendChild(image);
-
-        //For each image, create a link to the project
-        const anchor = document.createElement("a");
-        anchor.className = "carousel__anchor";
-        anchor.href = linkOfProject[i].URL;
-        anchor.target = "_blank";
-        image.appendChild(anchor);
-
-        //For each image indicate the project name if I need it
-        // const projectName = document.createElement("p");
-        // projectName.innerHTML = Object.keys(linkOfProject)[i];
-        // anchor.appendChild(projectName);
-
-    }
-    buttonLeft.onclick = function () {
+    // Add click event listener to the left arrow button
+    buttonLeft.addEventListener("click", () => {
         slideIndex--;
         if (slideIndex < 0) {
             slideIndex = numSlides - 1;
@@ -168,10 +251,10 @@ document.body.onload = function carousel() {
         slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
         slides.style.transition = "all 0.6s ease-in-out";
         clearInterval(autoscrolling);
+    });
 
-    };
-
-    buttonRight.onclick = function click() {
+    // Add click event listener to the right arrow button
+    buttonRight.addEventListener("click", () => {
         slideIndex++;
         if (slideIndex > numSlides - 1) {
             slideIndex = 0;
@@ -179,10 +262,19 @@ document.body.onload = function carousel() {
         slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
         slides.style.transition = "all 0.6s ease-in-out";
         clearInterval(autoscrolling);
-    };
+    });
+
+    // Restart autoplay when user clicks on any carousel slide
+    function restartCarousel() {
+        clearInterval(autoscrolling);
+        autoscrolling = setInterval(autoPlay, 3000);
+    }
+
+    buttonRight.addEventListener("click", restartCarousel)
+    buttonLeft.addEventListener("click", restartCarousel)
 
 
-};
+});
 
 //----- END CAROUSEL -----//
 
