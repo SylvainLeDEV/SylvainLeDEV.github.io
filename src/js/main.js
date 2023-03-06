edf9fdbfb8e7eea5275f296f0dd7c509e8fcd767
SylvainLeDev <dev.tess.sylvain@gmail.com>
2023-03-06 13:56:07 +0100

    update CV - improve JS - add my notion

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
-// Curtain menu
+let URL_FOLDER_DATA = '/assets/data/'
+// ---- End Environement ---- //
+
+// ---- Curtain menu ---- //
 function openNav() {
     document.getElementById("myOverlay").style.height = "100%";
 }
@@ -26,6 +29,8 @@ closeLinks.forEach(link => {
         // Scroll the page to that element
     });
 });
+// ---- End Curtain ---- //
+
 // ---- Navbar Scroll --------//
 let lastScrollTop = 0;
 const navbar = document.getElementById("myNavbar");
@@ -44,164 +49,185 @@ window.addEventListener("scroll", function (e) {
 // ---- End Navbar ----------//
 
 // ---- Description ---------//
-import {DESCRIPTION} from "/assets/data/description.js";
-
-const descriptionBlock = document.querySelector(".block__text");
-for (let i = 0; i < DESCRIPTION.length; i++) {
-    const paragraph = document.createElement("p");
-    paragraph.textContent = DESCRIPTION[i];
-    descriptionBlock.appendChild(paragraph);
-}
-// ---- End Description -----//
-import {LANGUAGE, TOOLS, SOFT_SKILLS} from "/assets/data/skills.js"
-
-const skillsContainer = document.querySelector(".skills__container-cards");
-const generateSkills = (skillsArray, skillClass, title) => {
-    const skillDiv = document.createElement("div");
-    skillDiv.classList.add("skill", skillClass);
-    const skillTitle = document.createElement("h4");
-    skillTitle.textContent = title;
-    skillDiv.appendChild(skillTitle);
-    skillsArray.forEach((skill) => {
-        const skillButton = document.createElement("button");
-        skillButton.classList.add("pill", "hover");
-        skillButton.textContent = skill;
-        skillDiv.appendChild(skillButton);
+const DESCRIPTION_MODULE_URL = `${URL_FOLDER_DATA}/description.js`;
+import(DESCRIPTION_MODULE_URL)
+    .then(module => {
+        const DESCRIPTION = module.DESCRIPTION;
+        const descriptionBlock = document.querySelector(".block__text");
+        for (let i = 0; i < DESCRIPTION.length; i++) {
+            const paragraph = document.createElement("p");
+            paragraph.textContent = DESCRIPTION[i];
+            descriptionBlock.appendChild(paragraph);
+        }
+    })
+    .catch(error => {
+        console.error(error)
     });
-    skillsContainer.appendChild(skillDiv);
-};
-generateSkills(LANGUAGE, "skill-language", "Languages");
-generateSkills(TOOLS, "skill-tools", "Tools");
-generateSkills(SOFT_SKILLS, "skill-soft-skills", "Soft Skills");
+// ---- End Description -----//
+const SKILLS_MODULE_URL = `${URL_FOLDER_DATA}/skills.js`;
+import(SKILLS_MODULE_URL)
+    .then(module => {
+        const LANGUAGE = module.LANGUAGE;
+        const TOOLS = module.TOOLS;
+        const SOFT_SKILLS = module.SOFT_SKILLS;
+        const skillsContainer = document.querySelector(".skills__container-cards");
+        const generateSkills = (skillsArray, skillClass, title) => {
+            const skillDiv = document.createElement("div");
+            skillDiv.classList.add("skill", skillClass);
+            const skillTitle = document.createElement("h4");
+            skillTitle.textContent = title;
+            skillDiv.appendChild(skillTitle);
+            skillsArray.forEach((skill) => {
+                const skillButton = document.createElement("button");
+                skillButton.classList.add("pill", "hover");
+                skillButton.textContent = skill;
+                skillDiv.appendChild(skillButton);
+            });
+            skillsContainer.appendChild(skillDiv);
+        };
+        generateSkills(LANGUAGE, "skill-language", "Languages");
+        generateSkills(TOOLS, "skill-tools", "Tools");
+        generateSkills(SOFT_SKILLS, "skill-soft-skills", "Soft Skills");
+    })
+    .catch(error => {
+        console.error("Error in file Skills : ", error)
+    });
 // ---- Pill of Skills ------//
 
 // ---- Experience --------- //
-import {CHRONOLOGIE} from "/assets/data/chronologie.js";
-
-const timelineDiv = document.querySelector(".timeline");
+const CHRONOLOGIE_MODULE_URL = `${URL_FOLDER_DATA}/chronologie.js`;
+import(CHRONOLOGIE_MODULE_URL)
+    .then(module => {
+        const CHRONOLOGIE = module.CHRONOLOGIE;
+        const timelineDiv = document.querySelector(".timeline");
 // Loop to display each element of CHRONOLOGIE.
-for (let i = 0; i < CHRONOLOGIE.length; i++) {
-    const element = CHRONOLOGIE[i];
+        for (let i = 0; i < CHRONOLOGIE.length; i++) {
+            const element = CHRONOLOGIE[i];
 
-    // Creating a div element for each element of CHRONOLOGIE.
-    const timelineExp = document.createElement("div");
-    timelineExp.classList.add("timeline-exp");
+            // Creating a div element for each element of CHRONOLOGIE.
+            const timelineExp = document.createElement("div");
+            timelineExp.classList.add("timeline-exp");
 
-    // add data-aos for animation
-    const divContent = document.createElement("div");
-    divContent.setAttribute("data-aos", i % 2 === 0 ? "zoom-in-left" : "zoom-in-right");
-    timelineExp.appendChild(divContent);
+            // add data-aos for animation
+            const divContent = document.createElement("div");
+            divContent.setAttribute("data-aos", i % 2 === 0 ? "zoom-in-left" : "zoom-in-right");
+            timelineExp.appendChild(divContent);
 
-    // add title
-    const title = document.createElement("h4");
-    title.textContent = element.title;
-    divContent.appendChild(title);
+            // add title
+            const title = document.createElement("h4");
+            title.textContent = element.title;
+            divContent.appendChild(title);
 
-    // add date
-    const date = document.createElement("h6");
-    date.textContent = element.date;
-    divContent.appendChild(date);
+            // add date
+            const date = document.createElement("h6");
+            date.textContent = element.date;
+            divContent.appendChild(date);
 
-    // Add content
-    const content = document.createElement("p");
-    content.textContent = element.content;
-    divContent.appendChild(content);
+            // Add content
+            const content = document.createElement("p");
+            content.textContent = element.content;
+            divContent.appendChild(content);
 
-    // Adding the div element to the timeline div.
-    timelineDiv.appendChild(timelineExp);
-}
-// ---- Experience ---------//
+            // Adding the div element to the timeline div.
+            timelineDiv.appendChild(timelineExp);
+        }
+    })
+    .catch(error => {
+        console.error("Error in file chronologie : ", error)
+    });
+
+// ---- End Experience ---------//
 
 //--------- Carousel ------//
-import {PROJECTS} from "/assets/data/project.js";
+const PROJECTS_MODULE_URL = `${URL_FOLDER_DATA}/project.js`;
+import(PROJECTS_MODULE_URL)
+    .then(module => {
+        const PROJECTS = module.PROJECTS;
+        const buttonRight = document.querySelector(".carousel__arrow-right");
+        const buttonLeft = document.querySelector(".carousel__arrow-left");
+        const slides = document.getElementById("carousel-items");
 
-document.addEventListener("DOMContentLoaded", () => {
+        let slideIndex = 0;
+        const numSlides = PROJECTS.length;
 
-    const buttonRight = document.querySelector(".carousel__arrow-right");
-    const buttonLeft = document.querySelector(".carousel__arrow-left");
-    const slides = document.getElementById("carousel-items");
+        // Get the width of the carousel container
+        let containerCarousel = document.querySelector(".portfolio__container-carousel").clientWidth;
 
-    let slideIndex = 0;
-    const numSlides = PROJECTS.length;
+        // Update the carousel container width on window resize
+        window.addEventListener("resize", () => {
+            containerCarousel = document.querySelector(".portfolio__container-carousel").clientWidth;
+            slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
+        });
 
-    // Get the width of the carousel container
-    let containerCarousel = document.querySelector(".portfolio__container-carousel").clientWidth;
+        // Set the width of the carousel to hold all slides
+        slides.style.width = `${numSlides * 100}%`;
 
-    // Update the carousel container width on window resize
-    window.addEventListener("resize", () => {
-        containerCarousel = document.querySelector(".portfolio__container-carousel").clientWidth;
-        slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
-    });
+        // Create carousel slides and anchors dynamically
+        PROJECTS.forEach((project, i) => {
+            const image = document.createElement("div");
+            image.className = "carousel__image";
+            image.style.backgroundImage = `url(./assets/img/carousel/${project.IMG}.jpg)`;
 
-    // Set the width of the carousel to hold all slides
-    slides.style.width = `${numSlides * 100}%`;
+            const anchor = document.createElement("a");
+            anchor.className = "carousel__anchor";
+            anchor.href = project.URL;
+            anchor.target = "_blank";
+            image.appendChild(anchor);
 
-    // Create carousel slides and anchors dynamically
-    PROJECTS.forEach((project, i) => {
-        const image = document.createElement("div");
-        image.className = "carousel__image";
-        image.style.backgroundImage = `url(./assets/img/carousel/${project.IMG}.jpg)`;
+            // Uncomment below to add project name to the anchor
+            // const projectName = document.createElement("p");
+            // projectName.innerHTML = Object.keys(PROJECTS)[i];
+            // anchor.appendChild(projectName);
 
-        const anchor = document.createElement("a");
-        anchor.className = "carousel__anchor";
-        anchor.href = project.URL;
-        anchor.target = "_blank";
-        image.appendChild(anchor);
+            slides.appendChild(image);
+        });
 
-        // Uncomment below to add project name to the anchor
-        // const projectName = document.createElement("p");
-        // projectName.innerHTML = Object.keys(PROJECTS)[i];
-        // anchor.appendChild(projectName);
+        // Function to play the carousel automatically
+        const autoPlay = () => {
+            slideIndex++;
+            if (slideIndex > numSlides - 1) {
+                slideIndex = 0;
+            }
+            slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
+            slides.style.transition = "all 0.6s ease-in-out";
+        };
 
-        slides.appendChild(image);
-    });
+        let autoscrolling = setInterval(autoPlay, 3000);
 
-    // Function to play the carousel automatically
-    const autoPlay = () => {
-        slideIndex++;
-        if (slideIndex > numSlides - 1) {
-            slideIndex = 0;
+        // Add click event listener to the left arrow button
+        buttonLeft.addEventListener("click", () => {
+            slideIndex--;
+            if (slideIndex < 0) {
+                slideIndex = numSlides - 1;
+            }
+            slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
+            slides.style.transition = "all 0.6s ease-in-out";
+            clearInterval(autoscrolling);
+        });
+
+        // Add click event listener to the right arrow button
+        buttonRight.addEventListener("click", () => {
+            slideIndex++;
+            if (slideIndex > numSlides - 1) {
+                slideIndex = 0;
+            }
+            slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
+            slides.style.transition = "all 0.6s ease-in-out";
+            clearInterval(autoscrolling);
+        });
+
+        // Restart autoplay when user clicks on any carousel slide
+        function restartCarousel() {
+            clearInterval(autoscrolling);
+            autoscrolling = setInterval(autoPlay, 3000);
         }
-        slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
-        slides.style.transition = "all 0.6s ease-in-out";
-    };
 
-    let autoscrolling = setInterval(autoPlay, 3000);
-
-    // Add click event listener to the left arrow button
-    buttonLeft.addEventListener("click", () => {
-        slideIndex--;
-        if (slideIndex < 0) {
-            slideIndex = numSlides - 1;
-        }
-        slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
-        slides.style.transition = "all 0.6s ease-in-out";
-        clearInterval(autoscrolling);
+        buttonRight.addEventListener("click", restartCarousel)
+        buttonLeft.addEventListener("click", restartCarousel)
+    })
+    .catch(error => {
+        console.error("Error in file project : ", error)
     });
-
-    // Add click event listener to the right arrow button
-    buttonRight.addEventListener("click", () => {
-        slideIndex++;
-        if (slideIndex > numSlides - 1) {
-            slideIndex = 0;
-        }
-        slides.style.transform = `translate(-${slideIndex * containerCarousel}px)`;
-        slides.style.transition = "all 0.6s ease-in-out";
-        clearInterval(autoscrolling);
-    });
-
-    // Restart autoplay when user clicks on any carousel slide
-    function restartCarousel() {
-        clearInterval(autoscrolling);
-        autoscrolling = setInterval(autoPlay, 3000);
-    }
-
-    buttonRight.addEventListener("click", restartCarousel)
-    buttonLeft.addEventListener("click", restartCarousel)
-
-
-});
-
 //----- END CAROUSEL -----//
 
 
@@ -209,7 +235,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // import Typed from '/assets/typed.js/lib/typed.js';
 let typed = new Typed('.typed', {
-    strings: ["Java Script", "React JS", "Node JS", "HTML / CSS"],
+    strings: ["Java Script", "React JS", "Node JS", "HTML / CSS", "AWS"],
     typeSpeed: 40,
     backSpeed: 100,
     fadeOut: true,
